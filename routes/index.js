869e3b3fb251797b24022c369fe60b36e8d9505c
haru22 <y.a.m.aa0867@gmail.com>
2018-07-31 17:10:20 -0700

    change bg

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// load website
+
+router.get('/about', function(req, res) {
+   res.render('about',{});
+});
+router.get('/history', function(req, res) {
+   res.render('history', { });
+});
+router.get('/cambodia', function(req, res) {
+   res.render('cambodia', { });
+});
+
 module.exports = router;
